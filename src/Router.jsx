c8fe815cc99ee1363892c3babbaffed5dade4207
Dohu Jeon <adc0612@gmail.com>
2022-09-20 17:00:16 +0900

    feat: install router and mui / initial setting

---
+++ src/Router.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Home from './routes/Home';
+import Question from './routes/Question';
+import Result from './routes/Result';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} exact />
+        <Route path='/question' element={<Question />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
